cbe6b56b092d7ca9f2916f87db973e3bcdc7d6d6
Sunny <sunnyjangra12hr@gmail.com>
2026-02-16 19:22:17 +0530

    fix crash in settings

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -121,19 +121,8 @@ try:
 except Exception as e:
     print("Superuser creation skipped:", e)
 
-portfolio_site/settings.py
 import os
 if os.environ.get("RENDER"):
     from django.core.management import call_command
     call_command("migrate")
     call_command("createsu")
-
-
-
-
-
-
-
-
-
-
