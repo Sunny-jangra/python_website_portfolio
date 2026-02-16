d9f2397c52e1f23a2c4477c30214e8afff990fad
Sunny <sunnyjangra12hr@gmail.com>
2026-02-16 19:36:17 +0530

    added auto superuser command

--- projects/management/commands/createsu.py
+++ projects/management/commands/createsu.py
@@ -6,14 +6,22 @@ class Command(BaseCommand):
     help = "Create superuser automatically"
 
     def handle(self, *args, **kwargs):
-        User = get_user_model()
-
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
+        if not username or not email or not password:
+            self.stdout.write(self.style.WARNING("Superuser env variables not found"))
+            return
+
+        User = get_user_model()
+
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            print("Superuser created")
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created successfully"))
         else:
-            print("Superuser already exists")
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
